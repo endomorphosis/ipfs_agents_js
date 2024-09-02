f78b130539a92ad98e9a50660f24d3a033f06763
endomorphosis <starworks5@gmail.com>
2024-09-01 20:43:29 -0700

    Refactor ipfs_agents_js imports and exports

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export * from './ipfs_agents_js/ipfs_agents.js';
+import { ipfsAgentsJs } from './ipfs_agents_js/ipfs_agents.js';
+const ipfsAgents = new ipfsAgentsJs();
+export { ipfsAgents };
+export { default as ipfsAgents } from './ipfs_agents_js/ipfs_agents.js';
+export { ipfsAgentsJs } from './ipfs_agents_js/ipfs_agents.js'; 
+export { ipfsAgentsJS as default } from './ipfs_agents_js/ipfs_agents.js';
+import { test_ipfs_agents_js } from './ipfs_agents_js/ipfs_agents.js';
+const test = new test_ipfs_agents_js();
+export { test };
